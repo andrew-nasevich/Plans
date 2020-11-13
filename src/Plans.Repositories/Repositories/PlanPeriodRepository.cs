604c6a3644004f8c19feab46a44af77007d5ad76
Andrew <nasevichandrey@gmail.com>
2020-11-13 17:20:35 +0300

    Specific Repositories were added

---
+++ src/Plans.Repositories/Repositories/PlanPeriodRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Plans.DomainModel.Plans;
+using Repositories;
+using System.Linq;
+
+namespace Plans.Repositories.Repositories
+{
+    public class PlanPeriodRepository : Repository<PlanPeriod>
+    {
+        public PlanPeriodRepository(DbContext context)
+           : base(context)
+        {
+
+        }
+
+        protected override IQueryable<PlanPeriod> GetAllQuery()
+        {
+            return GetQuery(pp => pp.Plan);
+        }
+    }
+}
---
+++ src/Plans.Repositories/Repositories/PlanRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Plans.DomainModel.Plans;
+using Repositories;
+using System.Linq;
+
+namespace Plans.Repositories.Repositories
+{
+    public class PlanRepository : Repository<Plan>
+    {
+        public PlanRepository(DbContext context)
+           : base(context)
+        {
+
+        }
+
+        protected override IQueryable<Plan> GetAllQuery()
+        {
+            return GetQuery(p => p.User);
+        }
+    }
+}
