54a24108517b4b919f6747fbc54dd0d708bfda1f
Andrew <nasevichandrey@gmail.com>
2020-11-13 17:43:02 +0300

    PlansUnitOfWork was added

---
+++ src/Plans.Repositories/UnitsOfWork/PlansUnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using Plans.DomainModel.Plans;
+using Plans.Repositories.DbContexts;
+using Plans.Repositories.Repositories;
+using Repositories;
+using Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+
+namespace Plans.Repositories.UnitsOfWork
+{
+    public class PlansUnitOfWork: UnitOfWork<PlansDbContext>
+    {
+        private readonly IDictionary<Type, Type> _repositoriesMapping;
+
+
+        public PlansUnitOfWork(PlansDbContext context)
+            :base(context)
+        {
+            _repositoriesMapping = new Dictionary<Type, Type>()
+            {
+                { typeof(Plan), typeof(PlanRepository) },
+                { typeof(PlanPeriod), typeof(PlanPeriodRepository) }
+            };
+        }
+
+
+        protected override IRepository<T> CreateRepository<T>()
+        {
+            if(_repositoriesMapping.TryGetValue(typeof(T), out var repositoryType))
+            {
+                return (IRepository<T>) Activator.CreateInstance(repositoryType, Context);
+            }
+
+            return base.CreateRepository<T>();
+        }
+    }
+}
---
+++ src/Plans.Repositories/UnitsOfWork/PlansUnitOfWorkFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Plans.Repositories.DbContexts;
+using Repositories.Interfaces;
+
+namespace Plans.Repositories.UnitsOfWork
+{
+    public class PlansUnitOfWorkFactory
+    {
+        public IUnitOfWork CreateSchoolScheduleUnitOfWork(IConfiguration configuration)
+        {
+            var context = new PlansDbContext(configuration);
+
+            return new PlansUnitOfWork(context);
+        }
+    }
+}
